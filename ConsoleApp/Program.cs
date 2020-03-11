5d52ba67df800985a879d9b1913f117346f956f7
BIOSYSTEM <46874046+lilbios@users.noreply.github.com>
2020-03-11 23:15:54 +0200

    fixed GroupIntArray method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,11 +9,11 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             //Console.WriteLine(Task.Factorial(7));
-            //var array = new []{ 1, 2, 3, 4,5,6,7,8,9,10 };
-            //Task.GroupIntArray(array);
+            var array = new []{ 1, 2, 3, 4,5,6,7,8,9,10 };
+            Console.WriteLine(Task.GroupIntArray(array));
 
          
-            Console.WriteLine(Task.Fibonacci(1));
+            
            
         }
     }
--- ConsoleApp/Task.cs
+++ ConsoleApp/Task.cs
@@ -10,30 +10,36 @@ namespace ConsoleApp
     {
         public static int Factorial(int number) => Enumerable.Range(1, number).Aggregate((x, y) => x * y);
 
-        public static int Fibonacci(int n)=> Enumerable.Range(1, n)
+        public static int Fibonacci(int n) => Enumerable.Range(1, n)
             .Aggregate(new KeyValuePair<int, int>(0, 1), (seq, index) => new KeyValuePair<int, int>(seq.Value, seq.Key + seq.Value)).Value;
-           
 
-        public static void GroupIntArray(int[] array)
+
+        public static string GroupIntArray(int[] array)
         {
-
+            int numberOfGroups = 0;
+            var strb = new StringBuilder();
             var query =
                 from number in array
                 group number by number % 2 == 0 into g
                 select g;
 
-            string msg = "Not Even";
-
+            strb.Append("Not Even numbers:");
             foreach (var group in query)
             {
-                Console.WriteLine(msg);
                 foreach (var number in group)
                 {
-                    Console.WriteLine(number);
+                    strb.Append($" {number} ");
                 }
-                msg = "Even";
+                ++numberOfGroups;
+                if (numberOfGroups != 2)
+                {
+                    strb.Append("\n");
+                    strb.Append("Even numbers:");
+                }
+               
             }
-        }
+            return strb.ToString();
 
+        }
     }
 }
